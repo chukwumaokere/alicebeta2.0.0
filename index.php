33de1f432f92aa0cfa4e379e3633cb825db3abaa
chukwumaokere <chukwumaokere@yahoo.com>
2018-04-01 23:16:59 +0000

    adding new alice framework

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>test</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" href="build/botui.min.css">
+  <link rel="stylesheet" href="build/botui-theme-default.css">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <div class="botui-app-container" id="hello-world">
+    <bot-ui></bot-ui>
+  </div>
+  <script src="https://cdn.jsdelivr.net/vue/2.0.5/vue.min.js"></script>
+  <script src="build/botui.js"></script>
+  <script src="script.js"></script>
+
+</body>
+</html>
